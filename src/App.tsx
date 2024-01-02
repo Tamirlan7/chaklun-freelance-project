b463394404d70990d2eb0846ef72e00f1933730e
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2024-01-02 20:31:35 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from "react";
+import Router from './routes/Router'
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import TopHeader from "./components/TopHeader/TopHeader";
+import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [onBurgerMenuActive, setOnBurgerMenuActive] = useState<boolean>(false)
+    const topHeaderRef = useRef<HTMLDivElement>(null)
+    const headerRef = useRef<HTMLHeadElement>(null)
+
+    useEffect(() => {
+        window.addEventListener('scroll', moveHeader)
+
+        return () => {
+            window.removeEventListener('scroll', moveHeader)
+        }
+    })
+
+    const onBurgerBtnClick = () => {
+        setOnBurgerMenuActive(true)
+    }
+
+    const closeBurgerMenu = () => {
+        setOnBurgerMenuActive(false)
+    }
+
+    function moveHeader() {
+        if (headerRef.current && topHeaderRef.current) {
+            const headersTotalHeight: number = headerRef.current.clientHeight + topHeaderRef.current.clientHeight
+
+            if (window.scrollY > headersTotalHeight) {
+                headerRef.current.className = 'header'
+            } else {
+                headerRef.current.className = ''
+            }
+        }
+    }
+
+    return (
+        <>
+            <div ref={topHeaderRef}>
+                <TopHeader/>
+            </div>
+
+            <div className="content-wrapper">
+                <header ref={headerRef}>
+                    <Header onBurgerBtnClicked={onBurgerBtnClick} />
+                </header>
+                <div className="main-and-footer">
+                    <main>
+                        <Router/>
+                    </main>
+
+                    <footer>
+                        <Footer/>
+                    </footer>
+                </div>
+            </div>
+            <BurgerMenu
+                active={onBurgerMenuActive}
+                deactivate={closeBurgerMenu}
+            />
+        </>
+    )
 }
 
-export default App;
+export default App
